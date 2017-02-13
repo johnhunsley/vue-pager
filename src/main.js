94f1232c9c2c8db056810801902e58a7bbef0e31
johnhunsley <snowball33>
2017-02-13 16:41:05 +0000

    moved to example

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueResource from 'vue-resource'
-import App from './App'
+import App from './example/App'
 
 Vue.use(VueResource)
 
